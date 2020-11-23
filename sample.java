ab887e1555d7f2c0bf8dcd05c3c58d859edb14dd
Work Account <workaccount@Vanis-MacBook-Pro.local>
2020-11-23 17:24:18 -0500

    done stack

--- sample.java
+++ sample.java
@@ -1,7 +1,40 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : N^2
+// Space Complexity : N
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : 
 
 
 // Your code here along with comments explaining your approach
+//if the temprate of next day is greater than current then take difference of index and add to result array
+public int[] dailyTemperatures(int[] T) {
+    int [] result = new int[T.length];
+    for(int i=0;i<T.length;i++){
+        for(int j=i+1;j<T.length;j++){
+            if (T[i]<T[j]){
+                result[i] = j-i;
+                break;
+            }
+        }
+    }
+  return result;  
+}  
+}
+O(N) ,O(N)
+//Optimized Solution
+ //only add element idf the next temp is higher than current and manipulate array
+ public int[] dailyTemperatures(int[] T) {
+    int [] result = new int[T.length];
+    Stack<Integer> stack = new Stack<>();
+    for(int i=0;i<T.length;i++){
+        while(!stack.isEmpty() && T[stack.peek()]<T[i]){
+           
+            //add result in array
+            result[stack.peek()] = i-stack.pop();
+            
+        }
+        stack.push(i);
+    }
+  return result;  
+}
+}
+
